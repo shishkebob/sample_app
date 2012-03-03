9be82c71e3d19054151b11f844838c72adc4cffa
Huachen Li <huachen.li@gmail.com>
2012-03-03 02:05:23 -0500

    Changed spork pre-fork and uploaded web proxy download

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,6 +18,10 @@ Spork.prefork do
     config.mock_with :rspec
     config.fixture_path = "#{::Rails.root}/spec/fixtures"
     config.use_transactional_fixtures = true
+    
+    def test_sign_in(user)
+      controller.sign_in(user)
+    end
    
     # Needed for Spork
     ActiveSupport::Dependencies.clear
@@ -63,16 +67,16 @@ end
 
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
-ENV["RAILS_ENV"] ||= 'test'
-require File.expand_path("../../config/environment", __FILE__)
-require 'rspec/rails'
-require 'rspec/autorun'
+# ENV["RAILS_ENV"] ||= 'test'
+# require File.expand_path("../../config/environment", __FILE__)
+# require 'rspec/rails'
+# require 'rspec/autorun'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
-Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
-
-RSpec.configure do |config|
+# Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+# 
+# RSpec.configure do |config|
   # ## Mock Framework
   #
   # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
@@ -82,19 +86,19 @@ RSpec.configure do |config|
   # config.mock_with :rr
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  # config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  # config.use_transactional_fixtures = true
 
   # If true, the base class of anonymous controllers will be inferred
   # automatically. This will be the default behavior in future versions of
   # rspec-rails.
-  config.infer_base_class_for_anonymous_controllers = false
+  # config.infer_base_class_for_anonymous_controllers = false
   
-  def test_sign_in(user)
-    controller.sign_in(user)
-  end
-end
+  # def test_sign_in(user)
+    # controller.sign_in(user)
+  # end
+# end
